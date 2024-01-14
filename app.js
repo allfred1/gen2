ad2d2201544ae6210b3cc9bc384aeda31a84cdf4
allfred1 <franigoyt@gmail.com>
2024-01-14 18:01:25 +0500

    Ver 0.1 Tested

---
+++ app.js
@@ -0,0 +1,102 @@
+const russianAlphabet = "ᴀбʙгдᴇжзийᴋлмʜᴏпᴘᴄʏȹxцчшщъыьэюя";
+const englishAlphabet = "ᴀʙᴄᴅᴇғɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxʏᴢ";
+
+const russian = {
+  А: "ᴀ",
+  Б: "б",
+  В: "ʙ",
+  Г: "г",
+  Д: "д",
+  Е: "ᴇ",
+  Ж: "ж",
+  З: "з",
+  И: "и",
+  Й: "й",
+  К: "ᴋ",
+  Л: "л",
+  М: "м",
+  Н: "ʜ",
+  О: "ᴏ",
+  П: "п",
+  Р: "ᴘ",
+  С: "ᴄ",
+  Т: "т",
+  У: "ʏ",
+  Ф: "ȹ",
+  Х: "x",
+  Ц: "ц",
+  Ч: "ч",
+  Ш: "ш",
+  Щ: "щ",
+  Ъ: "ъ",
+  Ы: "ы",
+  Ь: "ь",
+  Э: "э",
+  Ю: "ю",
+  Я: "я",
+};
+
+const english = {
+  A: "ᴀ",
+  B: "ʙ",
+  C: "ᴄ",
+  D: "ᴅ",
+  E: "ᴇ",
+  F: "ғ",
+  G: "ɢ",
+  H: "ʜ",
+  I: "ɪ",
+  J: "ᴊ",
+  K: "ᴋ",
+  L: "ʟ",
+  M: "ᴍ",
+  N: "ɴ",
+  O: "ᴏ",
+  P: "ᴘ",
+  Q: "ǫ",
+  R: "ʀ",
+  S: "s",
+  T: "ᴛ",
+  U: "ᴜ",
+  V: "ᴠ",
+  W: "ᴡ",
+  X: "x",
+  Y: "ʏ",
+  Z: "ᴢ",
+};
+
+const input = document.getElementById("input_zone");
+const output = document.getElementById("output_zone");
+const btnConvert = document.getElementById("Execute");
+
+btnConvert.addEventListener("click", () => {
+  let convertedText = convertText(input.value);
+  output.innerHTML = convertedText;
+});
+
+function convertText(text) {
+  let words = text.split(" ");
+
+  let result = "";
+
+  for (let word of words) {
+    let upperWord = word.toUpperCase();
+    let convertedWord = "";
+
+    for (let i = 0; i < upperWord.length; i++) {
+      let char = upperWord[i];
+
+      if (russian[char]) {
+        convertedWord += russian[char];
+      } else if (english[char]) {
+        convertedWord += english[char];
+      } else {
+        convertedWord += char;
+      }
+    }
+
+    result += convertedWord + " ";
+  }
+
+  return result;
+}
